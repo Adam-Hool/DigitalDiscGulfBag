7f19047fb81a95ca9ed8ba56e5b6ee8b96087a7a
Adam <adamhool414@gmail.com>
2016-12-02 15:32:51 -0600

    initalCommit

---
+++ DigitalDiscGulfBag/DigitalDiscGulfBag/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DigitalDiscGulfBag.Startup))]
+namespace DigitalDiscGulfBag
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
